7089f91db2498567eb4817a5eae26a24ffedbc51
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-12-05 00:34:22 -0600

    adds the stacks to to an array of stacks

--- day5part1/Solution.java
+++ day5part1/Solution.java
@@ -1,17 +1,26 @@
 import java.io.*;
+import java.util.*;
 
 public class Solution {
 	String path;
+	List<Stack<Character>> elfStacks = new ArrayList<Stack<Character>>();
 
 	public Solution(String path) {
 		this.path = path;
+	}
+
+	public void initializeStacks(int total) {
+		while (total > 0) {
+			elfStacks.add(new Stack<Character>());
+			total--;
+		}
 	}
 
 	public void readImage() {
 		try (BufferedReader br = new BufferedReader(new FileReader(this.path))) {
 			String line = br.readLine();
 			int totalStacks = (line.length() + 1) / 4;
-			System.out.println("there are " + totalStacks + " total stacks");
+			initializeStacks(totalStacks);
 			while (!line.startsWith(" 1")) {
 				readImageLevel(line);
 				line = br.readLine();
@@ -24,6 +33,7 @@ public class Solution {
 	public void readImageLevel(String line) {
 		for (int i = 1, stack = 0; i < line.length(); i += 4, stack++) {
 			if (line.charAt(i) != ' ') {
+				elfStacks.get(stack).push(line.charAt(i));
 				System.out.println("adding " + line.charAt(i) + " to stack" + (stack + 1));
 			}
 		}
@@ -32,9 +42,7 @@ public class Solution {
 	public void readFile() {
 		try (BufferedReader br = new BufferedReader(new FileReader(this.path))) {
 			String line = br.readLine();
-			int totalStacks = (line.length() + 1) / 4;
 			while (!line.startsWith(" 1")) { // read until the end of the image
-				System.out.println("it has " + totalStacks);
 				System.out.println(line);
 				line = br.readLine();
 			}
@@ -47,5 +55,10 @@ public class Solution {
 		System.out.println("Day 5");
 		Solution solving = new Solution("test.txt");
 		solving.readImage();
+		// checking if the elf stacks were added
+
+		for (Stack<Character> elfStack : solving.elfStacks) {
+			System.out.println(Arrays.toString(elfStack.toArray()));
+		}
 	}
 }
