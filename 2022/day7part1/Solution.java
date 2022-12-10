21616fb3b415007e5cab270eb8ad3d3888555b53
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-12-09 21:18:38 -0600

    day7 complete

--- 2022/day7part1/Solution.java
+++ 2022/day7part1/Solution.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 class Directory {
     String name;
     int size;
-    int globalSize;
     Directory parent = null;
     List<Directory> subDirectories = new ArrayList<>();
 
@@ -27,10 +26,23 @@ class Directory {
     Directory findSubDirectory(String name) {
         return this.subDirectories.stream().filter(subDir -> name.equals(subDir.name)).findFirst().orElse(null);
     }
+
+    int getGlobalSize() {
+        if (this.subDirectories.size() == 0) {
+            return this.size;
+        } else {
+            int subTotal = 0;
+            for (Directory directory : subDirectories) {
+                subTotal += directory.getGlobalSize();
+            }
+            return subTotal + this.size;
+        }
+    }
 }
 
 public class Solution {
     String path;
+    List<Integer> answer = new ArrayList<>();
 
     public Solution(String input) {
         this.path = input;
@@ -45,11 +57,10 @@ public class Solution {
         return m.matches();
     }
 
-    public void readFile() {
-
+    public Directory createDirectoryTree() {
+        Directory root = new Directory("/", null);
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
-            Directory root = new Directory("/", null);
             Directory current = root;
             while (line != null) {
                 String[] words = line.split(" ");
@@ -79,11 +90,39 @@ public class Solution {
         } catch (Exception e) {
             System.out.println(e);
         }
+        return root;
+    }
+
+    public void traverseTree(Directory root) {
+        if (root == null) {
+            return;
+        }
+
+        int subCount = root.subDirectories.size();
+        for (int i = 0; i < subCount; i++) {
+            traverseTree(root.subDirectories.get(i));
+        }
+        int total = root.getGlobalSize();
+        answer.add(total);
+        System.out.println("The node: " + root.name + " has size of " + total);
     }
 
     public static void main(String[] args) {
         System.out.println("Day7");
-        Solution day7 = new Solution("test.txt");
-        day7.readFile();
+        Solution day7 = new Solution("input.txt");
+        Directory tree = day7.createDirectoryTree();
+        System.out.println(tree.getGlobalSize());
+        day7.traverseTree(tree);
+        int total = day7.answer.get(day7.answer.size() - 1);
+        long available = 70000000 - total;
+        System.out.println("total is " + total);
+        Collections.sort(day7.answer);
+        for (Integer file : day7.answer) {
+            System.out.println(file);
+            if (available + file > 30000000) {
+                System.out.println("The answer is " + file);
+                break;
+            }
+        }
     }
 }
